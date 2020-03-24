9b2bedcac9e3ab2c7a7299f8f0cd7f11f33ef297
HongDaSeon <HongDaSeon@users.noreply.github.com>
2020-03-24 15:38:22 +0900

    Add files via upload

---
+++ python-side/comm_python-side.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+################################################################################################################################################################################ -ohh`  #####################                
+#                                                                                                                                                                             -ohNMMMMd`                    #
+#                                                                                                                                                                         -ohNMMMMMmMMMd`                   #
+#                                                                                                                                                                     -ohNMMMMNho-  yMMMd`                  #
+#                                                                                                                                                                 -ohNMMMMNho-       yMMMd`                 #
+#                                                                                                                                                             -ohNMMMMNho-            yMMMd`                #
+#                                                                                                                                                         -+hNMMMMMdo-                 yMMMd`               #
+#                                                                                                                                                     -+hNMMMMMdo:`                     yMMMd`              #
+#                                                                                                                                                 -+hNMMMMMho-                           sMMMd`             #
+#                                                                                                                                                `dMMMMho-                                sMMMd`            #
+#                                                                                                                                                 `dMMMo                                   sMMMd`           #
+#                                                                                                                                                  `dMMMs                                   sMMMd`          #
+#                                                                                                                                                   `dMMMs                                   sMMMm`         #
+#                                                                                                                                                    `dMMMo         `:osyyo/.                 sMMMm`        #
+#                                                                                                                                                     `dMMMo      `sNMMMMMMMMh-                sMMMm`       #
+#    MMMMMMMMo mMMMMNNds-    -yNMMMMNd-      sMMs                -MMh                                    `ddh                                          `dMMMo    `mMMMMMMMMMMMM/                sMMMm`      #
+#    MMM/----. mMMo--+mMMy  oMMN+.`./h:      sMMy      -+osso/.  -MMh-+so:    :osso/`  .++:`/+`-+osso/. `+MMN++/  -+sso/.  `++/`:+/++-   :++.           `dMMMo   oMMMMMMMMMMMMMm                 sMMMm.     #
+#    MMMyssss` mMM/   `mMM/.MMM-             sMMy      oysosmMM/ -MMNhydMMy -mMNsodMN/ /MMNMNN.oysosmMM/.yMMMyyo`dMMyohMMo .MMNNNN+NMN. -MMd             `dMMMs  oMMMMMMMMMMMMMm              ./ymMMMMm`    #
+#    MMMhyyyy` mMM/    dMMo-MMM`             sMMy      .+syhmMMs -MMh   NMM.hMM/  `MMM`/MMh    .+syhmMMs `MMN   sMMo   mMM-.MMm`   :MMh dMN.              `dMMMs `mMMMMMMMMMMMM:          `/ymMMMMMms/`     #
+#    MMM.      mMM/  `oMMm` mMMy`    /.      sMMy     :MMd:-oMMs -MMh  `NMM`hMM/  `MMN`/MMy   :MMd:-oMMs `MMN   oMMs   mMM-.MMm     oMMdMM:                `dMMMs `sNMMMMMMMMh-       `/smMMMMMms/`         #
+#    MMM.      mMMmdmMMNs`  `yMMNdhdNM:      sMMNmmmmd-MMNssmMMs -MMNyyNMN/ .mMNysmMN/ /MMy   -MMNssmMMs  mMMhss`hMMysdMMo .MMm      hMMMs                  `dMMMs   -+ssso/`     `/smMMMMMms/`             #
+#    ///`      //////:.       `:+oo+:.       -//////// .+o+:.//- `//::++/`    -+oo+:`  .//-    .+o+:.//-   :+o+:  ./ooo/`  `///      +MMd                    `dMMMs           ./smMMMMMms/`                 #
+#                                                                                                                                   .NMN.                     `dMMMs      ./ymMMMMMms/`                     #
+#                                                                                                                                   `..`                       `hMMMs `/smMMMMMmy/`                         #
+#                                                                                                                                                               `hMMMmMMMMMmy/.                             #
+#                                             T  H  E    M  O  T  I  O  N    T  E  C  H  N  O  L  O  G  Y    I  N  N  O  V  A  T  I  O  N  S                     `hMMMMmy/.                                 #
+#                                                                                                                                                                 `yy/.                                     #
+#                                                                                                                                                                                                           #
+#   ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------  #
+#                                                                                                                                                                                                           #
+#                    `++++++/-`    `+++++++-   -+ooo+:  .++.     ./oooo+-   +++`     :+/   +++++++-  -++++++++++++//:.                                                                                      #
+#                    .MMmyyhmMMh-  `MMmyyyy: -mMmsosyy  /MM:   +mMmyssydh   MMMN:    hMm   NMmyyyy/  oMMMMMMMMMMMMMMMMMmy+.                                                                                 #
+#                    .MMo    -mMN. `MMo      sMM/       /MM:  hMN/          MMhNMo   hMm   NMy       oMMMMMMMMMMMMMMMMMMMMMh:                                                                               #
+#                    .MMo     :MMo `MMmyyyy` `yMMmy+.   /MM: :MMo  `+++++.  MMs.dMh` hMm   NMmyyyy.  oMMMMM+     `.:odMMMMMMMy                                                                              #
+#                    .MMo     /MM+ `MMh++++`   `:sdMMy  /MM: :MMo  `yydMM:  MMs  sMm-hMm   NMd++++`  oMMMMM+          .yMMMMMMy                                                                             #
+#                    .MMo   `+NMh  `MMo      .     sMM: /MM:  dMN/    /MM:  MMs   /NMmMm   NMy       oMMMMM+            +MMMMMM:                                                                            #
+#                    .MMNddmMMh/   `MMNdddds sNdyydMMy  /MM:  `omMNdhhNMM-  MMs    .dMMm   NMNddddy  oMMMMM+             hMMMMMy                                                                            #
+#                     :::::-.       :::::::- `-////-`   `::`     .:///:-`   ::.      ::-   :::::::-  oMMMMM+             +MMMMMm                                                                            #
+#                                                                                                    oMMMMM+             +MMMMMd                                                                            #
+#                                                                                .-`                 oMMMMM+             hMMMMMs                                                                            #
+#                                                                                dM:                 oMMMMM+            /MMMMMM. `mmm.    /hmmmm+ `mmmmmm`  /hmNNmy:   dmd.   -mh                           #
+#                                                                                dM:-//- `//`  `/:   oMMMMM+          `oMMMMMM/  hMoMd   -MM.   ` `MM`    `dMs.  -hMy  mMmN/  :Md                           #
+#                                                                                dMdo+hMy sMs  yM/   oMMMMM+       `:sNMMMMMm:  oMs yMo   sNNh+.  `MMysso +Mm     .MM. mM-sMs :Md                           #
+#                                                                                dM:  `MM` dM::Ms    oMMMMMmhhhhddNMMMMMMMNo`  :MMssyMM:    -omMy `MM:::- /MN`    -MM` mM- :Nd/Md                           #
+#                                                                                dMy..sMd  `mmNd     oMMMMMMMMMMMMMMMMMmy:    `NM+:::+MN`-+-.-sMm `MM/:::` yMd+::oNN/  mM-  .dMMd                           #
+#                                                                                os/shy/    :MN.     /hhhhhhhhhhhyso+:`       /s+     +s/.syhys+` `ssssss-  .+yhys/`   os.    oso                           #
+#                                                                                         /+mN-                                                                                                             #
+######################################################################################## /+:` ###############################################################################################################
+
+# miss_neuron.py
+#    Missile_torch-matlab_bridge
+#  
+
+import csv
+import sys
+import numpy as np
+import torch
+import torchvision
+import torch.nn as nn
+import torchvision.transforms as transforms
+import time
+import copy
+
+# basic initialization+++++++++++++++++++++++++++++++++++++++++++++
+slept = False
+animcounter = 0
+RX    = 'read_dat'
+TX   = 'write_dat'
+ok = 0
+weightfile      = 'D:\\My_work\\Deep\\missile_deep1\\Missile_Simulator_neurallink_2dim_PNG_\\pytorch\\missneuron.py'
+testfilename    = 'D:\\My_work\\Deep\\missile_deep1\\Missile_Simulator_neurallink_2dim_PNG_\\link.combuff'
+filename = testfilename
+prevstamp = 99
+autostop    = False
+
+# nn param
+gpu_num = 0
+
+
+Vm = 250.0
+
+
+# Normalization factor
+mean    = np.array([ 2.7582396e+04, -1.0712867e-01,  8.2476425e+01],dtype=np.float64)
+std     = np.array([ 2.1391297e+04,  9.7707397e-01,  5.1373589e+01],dtype=np.float64)
+
+# NN Device +++++++++++++++++++++++++++++++++++++++++++++++++++++
+device = ('cuda'+':'+ str(gpu_num)) if torch.cuda.is_available() else 'cpu'
+
+torch.manual_seed(777)
+if device == ('cuda'+ ':' + str(gpu_num)) :
+    torch.cuda.manual_seed_all(777)
+
+# NN initialization+++++++++++++++++++++++++++++++++++++++++++++++
+class DNN(nn.Module):
+    
+    def __init__(self):
+        super(DNN, self).__init__()
+
+        self.fc1     = nn.Linear(3, 27, bias = True)    
+        self.relu1   = nn.Tanh()
+      
+        self.fc2     = nn.Linear(27, 243, bias = True) 
+        self.relu2   = nn.Tanh()
+   
+        self.fc3     = nn.Linear(243, 81, bias = True)
+        self.relu3   = nn.Tanh()
+        
+        self.fc5     = nn.Linear(81, 81, bias = True)
+        self.relu5   = nn.Tanh()
+
+        self.fc6     = nn.Linear(81, 1, bias = True)
+
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+        torch.nn.init.xavier_uniform_(self.fc2.weight)
+        torch.nn.init.xavier_uniform_(self.fc3.weight)
+     
+        torch.nn.init.xavier_uniform_(self.fc5.weight)
+        torch.nn.init.xavier_uniform_(self.fc6.weight)
+
+        
+    def forward(self, x):
+        
+        
+        #out = out.view(out.size(0), -1)
+        #print(out.shape)
+        #print(x.shape)
+        
+        out = self.fc1(x)
+        out = self.relu1(out)
+        
+        out = self.fc2(out)
+        out = self.relu2(out)
+        
+        out = self.fc3(out)
+        out = self.relu3(out)
+
+        
+        out = self.fc5(out)
+        out = self.relu5(out)
+                
+        out = self.fc6(out)
+        #print(out.shape)
+        return out
+
+
+# Model initialization _++++++++++++++++++++++++++++++++++
+
+print ('device : ', device)
+model   = DNN().to(device)
+print ('pass1')
+loaded  = torch.load( weightfile, map_location= device )
+print ('pass1.5')
+model.load_state_dict(loaded)
+model.double()
+print ('pass2')
+model.eval()
+print ('Good2Go')
+
+def feed2NN(test_set):
+    with torch.no_grad():
+        TensorTesTseT       = torch.from_numpy(np.array([test_set]))
+        #print TensorTesTseT
+        ay_cmd_tensor       = model(TensorTesTseT.to(device))
+        ay_cmd_tensor       = copy.copy(ay_cmd_tensor.to('cpu'))
+        ay_cmd_numpy        = ay_cmd_tensor.numpy()
+        ay_cmd_numpy        = np.array(ay_cmd_numpy,dtype=np.float64)
+        
+        #print vel_cmd_numpy.shape
+        list_cmd            = ay_cmd_numpy.tolist()
+        
+        return list_cmd[0]
+
+def comlink(cmd, data=0, stamp=0, indata=0):
+    global filename
+    global Author
+    global Reader
+    if cmd == 'read_dat':
+        noflagcounter = 0
+        while True:
+            if noflagcounter >= 1000:
+                pass
+                #quit()
+            verif_counter = 0    
+            try:
+                verif_counter = 0
+                f       = open(filename,'r')
+                Reader  = csv.reader(f)
+                ok      = 1
+                Stamp   = np.zeros([1], dtype=np.float64)
+                indata  = np.zeros([2], dtype=np.float64)
+                
+                for row in Reader:
+                    verif_counter += 1   
+                    
+                    '''if len(row) > 4:
+                        ok = 0'''
+                    Stamp   = np.array(row[0:1], dtype=np.float64)
+                    indata  = np.array(row[1:3], dtype=np.float64)
+                    #print Stamp, indata
+                    break
+                if (verif_counter != 1):# & (abs(prevstamp-Stamp[0])<0.0001):
+                    ok = 0
+            except:
+                noflagcounter += 1
+
+            noflagcounter += 1
+            
+            if ok == 1:
+                break
+            
+        f.close()
+        return indata, Stamp
+
+    if cmd == 'write_dat':
+        f       = open(filename,'a')
+        Author  = csv.writer(f)
+        #print putdat
+        Author.writerow(data)
+
+        f.close()
+
+while True:
+    if animcounter == 0:
+        print ('running')
+        animcounter     = 1
+    indata, instamp     = comlink(RX)
+    if prevstamp != instamp:
+        #print instamp
+        normalval       = np.array([indata[0], indata[1], Vm],dtype=np.float64) - mean
+        normalval       = normalval / std
+        derived         = feed2NN(normalval)
+        putdat          = [instamp[0],derived[0],derived[0],999.9]
+        #print putdat
+        comlink(TX,putdat,stamp=instamp,indata=indata)
+        prevstamp       = instamp
+        noflagcounter   = 0
+        slept = False
+    noflagcounter += 1
+    if noflagcounter >= 1000:
+        time.sleep(0.001)
+        slept = True
+    if (noflagcounter >= 2000) & (slept == True) & (autostop == True):
+        quit()
+        a = 0
